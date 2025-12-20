424926e1f32cee363e56f2d91e2936a760a41f8a
Diego Delgadillo <drdg2005@gmail.com>
2025-12-19 19:46:30 -0600

    sum of 5 digits numbers
    Se añadió otro método

--- sum_of_digits_of_five_digits_number.c
+++ sum_of_digits_of_five_digits_number.c
@@ -8,7 +8,7 @@ int main() {
     int n;
     scanf("%d", &n);
     //Complete the code to calculate the sum of the five digits on n.
-    int c1,c2,c3,c4,c5,sum;
+    int c1=0,c2,c3,c4,c5,sum = 0;/*
     c1 = n / 10000;
     n = n % 10000; printf("->%d",n);
     c2 = n / 1000;
@@ -25,7 +25,17 @@ int main() {
     sum = c1+c2+c3+c4+c5;
 
     printf("\n%d\n%d\n%d\n%d\n%d\nSuma -> %d",c1,c2,c3,c4,c5,sum);
+*/
+    //Código alterno y reducido jej
+    for(int i = 100000;i>=10;i++){
+        i /= 10;
+        c1 = n / i;
+        n = n % i;
+        sum+=c1;
+        printf("\n%d",sum);
+    }
 
+    printf("\n\n%d",sum);
 
     return 0;
 }
